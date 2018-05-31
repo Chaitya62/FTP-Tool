954d49b0f4ddf6bb369d3940fa6fbffe8fc9fd94
Chaitya Shah <chaitya.shah@somaiya.edu>
2018-06-01 01:08:43 +0530

    Comment and refactor ftpClient.py

--- ftpClient.py
+++ ftpClient.py
@@ -11,6 +11,7 @@ PORT = 3721
 USERNAME = ''
 PASSWORD = ''
 
+# mutex
 lock = threading.Lock()
 
 
@@ -25,21 +26,7 @@ def printr(*args, **kwargs):
         print(*args, **kwargs)
         lock.release()
 
-def progress(curr, total, mn, mx):
 
-
-    i = int(float((curr-4096)/(total-4096))*mx) + mn
-
-    progress_bar = ""
-
-    progress_bar += "["
-    progress_bar += '#'*i
-    progress_bar += '.'*(mx-i)
-    progress_bar += ']'
-
-    #print("{} : {}".format(curr,total))
-
-    printr(progress_bar, end='\r')
 
 
 
@@ -53,6 +40,7 @@ class FTPClient(ftplib.FTP, threading.Thread):
         ftplib.FTP.__init__(self)
         threading.Thread.__init__(self)
 
+        # initialize
         self.host = host
         self.port = port
         self.username = username
@@ -60,47 +48,90 @@ class FTPClient(ftplib.FTP, threading.Thread):
         self.directory = directory
         self.os_path = os_path
         self.download_vars = dict()
-
-        # connect
-        self.connect(host, port)
-
-
-
-        # login
-        self.login(username, password)
-        self.curr_dir = None
-
         self.dirs = []
         self.files = []
         self.filesizes = []
 
 
-        # directory
+        # connect
+        self.connect(host, port)
+
+        # login
+        self.login(username, password)
+        self.curr_dir = None
 
 
 
+
+
+        # change directory
         self.cwd(directory)
 
 
+    def progress(self, curr, total, mn, mx):
+
+        """
+            This function prints the progress bar
+
+            curr : downloaded
+            total: total file size
+            mn: minimum progress bar scale
+            mx: maximum progress bar scale
+
+        """
+
+        i = int(float((curr-4096)/(total-4096))*mx) + mn
+
+        progress_bar = ""
+
+        progress_bar += "["
+        progress_bar += '#'*i
+        progress_bar += '.'*(mx-i)
+        progress_bar += ']'
+
+        #print("{} : {}".format(curr,total))
+
+        printr(progress_bar, end='\r')
 
 
     def cwd(self, dirname):
+
+        """
+            overrides the cwd function of ftplib.FTP to update curr_dir
+        """
+
         super().cwd(dirname)
         self.curr_dir = dirname
 
     def download_util(self, mess):
+
+        """
+
+            This function acts as a callback for `self.retrbinary`
+            used to get update the progress bar
+
+            mess: recieved message from the socket
+
+        """
 
         absFile = self.download_vars['absFile']
         size = self.download_vars['size']
         fx = self.download_vars['fx']
 
         file_stats = os.stat(absFile)
-        progress(file_stats.st_size, size, 1, 50)
+        self.progress(file_stats.st_size, size, 1, 50)
         fx.write(mess)
-
         return
 
+
+
     def _download(self, filename, size):
+
+        """
+            main file downloader
+            filename: duh!
+            size: filesize
+        """
 
         self.download_vars['absFile'] = os.path.join(self.os_path, filename)
 
@@ -111,11 +142,11 @@ class FTPClient(ftplib.FTP, threading.Thread):
         return
 
 
-
     def _get_name(self, line):
 
         """
             utils for getting file or directory name from a LIST line
+            line: passed from `_parse`
 
         """
 
@@ -142,6 +173,8 @@ class FTPClient(ftplib.FTP, threading.Thread):
         """
             utils for getting file size
 
+            line: passed from `_parse`
+
         """
 
         pattern = re.compile(r'[0-9]{2,}')
@@ -162,7 +195,7 @@ class FTPClient(ftplib.FTP, threading.Thread):
         """
 
         callback self.dir to get files and directories
-        line: argument passed by self.dir
+        line: argument passed by `self.dir`
 
         """
 
@@ -184,9 +217,23 @@ class FTPClient(ftplib.FTP, threading.Thread):
 
 
     def run(self):
+
+        """
+            Overrides the `run` function from threading.Thread
+
+        """
+
+
         self.download()
 
+    # TODO: Change function name
     def download(self):
+
+
+        """
+            Recursive function to download the directory
+        """
+
 
         self.dir(self._parse)
 
@@ -231,5 +278,3 @@ save_directory = '.\\testing ' # I am on windows :)
 
 ftp = FTPClient(HOST, PORT, USERNAME, PASSWORD, ftp_directory, save_directory)
 ftp.download()
-
-printr("DONE!!")
